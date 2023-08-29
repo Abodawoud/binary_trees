02a5ad005db167e1ac860cf01093e48b0f33b424
Abodawoud <yousefabodawoud@gmail.com>
2023-08-30 00:43:16 +0300

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+size_t binary_tree_height_perfect(const binary_tree_t *tree);
 /**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
  * @tree: Pointer to the root node of the tree to traverse
@@ -11,8 +12,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (!tree)
 		return (1);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = (int)binary_tree_height_perfect(tree->left);
+	right_height = (int)binary_tree_height_perfect(tree->right);
 
 	if (left_height != right_height)
 		return (0);
@@ -20,21 +21,17 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	binary_tree_is_perfect(tree->right));
 }
 /**
- * binary_h - Measures the height of a binary tree
- *
- * @tree: Pointer to the root node of the tree to traverse
- * Return: height
+ * binary_tree_height_perfect - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree
+ * Return: Height of the tree
  */
-size_t binary_h(const binary_tree_t *tree)
+size_t binary_tree_height_perfect(const binary_tree_t *tree)
 {
-	size_t left_height;
-	size_t right_height;
+	if (!tree)
+		return (-1);
 
-	if (tree == NULL)
-		return (0);
-
-	left_height = binary_h(tree->left);
-	right_height = binary_h(tree->right);
+	int left_height = binary_tree_height_perfect(tree->left);
+	int right_height = binary_tree_height_perfect(tree->right);
 
 	return (1 + max(left_height, right_height));
 }
@@ -51,19 +48,4 @@ size_t max(size_t a, size_t b)
 		return (a);
 	else
 		return (b);
-}
-/**
- * binary_tree_height - Measures the height of a binary tree
- *
- * @tree: Pointer to the root node of the tree to traverse
- * Return: height
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t height;
-
-	if (tree == NULL)
-		return (0);
-	height = binary_h(tree);
-	return (height - 1);
 }
